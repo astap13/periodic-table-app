b11e0e9019393205413ea307ac35b4663dabd969
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-09-19 02:18:25 +0200

    init component periodic-table

---
+++ src/app/periodic-table/periodic-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-periodic-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './periodic-table.component.html',
+  styleUrl: './periodic-table.component.scss'
+})
+export class PeriodicTableComponent {
+
+}
