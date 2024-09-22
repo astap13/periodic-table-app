b59640b34e2f0a78e925602f8a51facafa8a443a
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-09-22 02:50:44 +0200

    add filter

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -1,15 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PeriodicElement } from '../models/periodic-element.model';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatPaginator } from '@angular/material/paginator';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { EditDialogComponent } from '../edit-dialog/edit-dialog.component';
 import { RxState } from '@rx-angular/state';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { RxIf } from '@rx-angular/template/if';
 
 @Component({
@@ -21,27 +20,32 @@ import { RxIf } from '@rx-angular/template/if';
     MatFormFieldModule,
     MatProgressBarModule,
     ReactiveFormsModule,
-    RxIf
+    RxIf,
   ],
   providers: [RxState],
   templateUrl: './periodic-table.component.html',
-  styleUrl: './periodic-table.component.scss',
+  styleUrls: ['./periodic-table.component.scss'],
 })
 export class PeriodicTableComponent implements OnInit {
   constructor(
-    private state: RxState<{ periodicElementData: PeriodicElement[], showLoader: boolean }>
+    private state: RxState<{
+      periodicElementData: PeriodicElement[];
+      filteredData: PeriodicElement[];
+      showLoader: boolean;
+    }>
   ) {}
 
   ngOnInit(): void {
     this.state.set({
       periodicElementData: [],
+      filteredData: [],
       showLoader: true,
     });
     this.loadData();
     this.setupFilter();
   }
 
-  periodicElementData$ = this.state.select('periodicElementData');
+  periodicElementData$ = this.state.select('filteredData');
   showLoader$ = this.state.select('showLoader');
 
   ELEMENT_DATA: PeriodicElement[] = [
@@ -58,9 +62,6 @@ export class PeriodicTableComponent implements OnInit {
   ];
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-
-  dataSource!: MatTableDataSource<PeriodicElement, MatPaginator>;
-
   filterControl: FormControl = new FormControl('');
 
   readonly dialog = inject(MatDialog);
@@ -69,13 +70,10 @@ export class PeriodicTableComponent implements OnInit {
     setTimeout(() => {
       this.state.set({
         periodicElementData: this.ELEMENT_DATA,
+        filteredData: this.ELEMENT_DATA,
         showLoader: false,
       });
-    }, 3000)
-  }
-
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    }, 3000);
   }
 
   setupFilter() {
@@ -84,6 +82,21 @@ export class PeriodicTableComponent implements OnInit {
       .subscribe((filterValue: string) => {
         this.applyFilter(filterValue);
       });
+  }
+
+  applyFilter(filterValue: string) {
+    const filteredData = this.state
+      .get('periodicElementData')
+      .filter((element) => {
+        const searchTerm = filterValue.trim().toLowerCase();
+        return (
+          element.name.toLowerCase().includes(searchTerm) ||
+          element.weight.toString().includes(searchTerm) ||
+          element.symbol.toLowerCase().includes(searchTerm)
+        );
+      });
+
+    this.state.set({ filteredData });
   }
 
   openEditDialog(elementData: PeriodicElement) {
@@ -99,9 +112,12 @@ export class PeriodicTableComponent implements OnInit {
   }
 
   updateDataSource(updatedElement: PeriodicElement): void {
-    const updatedData = this.dataSource.data.map((element) =>
-      element.position === updatedElement.position ? updatedElement : element
-    );
-    this.dataSource.data = updatedData;
+    const updatedData = this.state
+      .get('periodicElementData')
+      .map((element) =>
+        element.position === updatedElement.position ? updatedElement : element
+      );
+    this.state.set({ periodicElementData: updatedData });
+    this.applyFilter(this.filterControl.value);
   }
 }
