84a3d8720fd99e03b5ad7533f245a965e6051266
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-09-22 03:35:29 +0200

    refactor:  changing date without mutable

--- src/app/periodic-table/periodic-table.state.ts
+++ src/app/periodic-table/periodic-table.state.ts
@@ -21,16 +21,29 @@ export class PeriodicTableStateService extends RxState<PeriodicTableState> {
     });
   }
 
-  loadData(elements: PeriodicElement[]) {
+  ELEMENT_DATA: PeriodicElement[] = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  ];
+
+  loadData() {
     this.set({
-      periodicElementData: elements,
-      filteredData: elements,
+      periodicElementData: this.ELEMENT_DATA,
+      filteredData: [...this.ELEMENT_DATA],
       showLoader: false,
     });
   }
 
   applyFilter(filterValue: string) {
-    const filteredData = this.get('periodicElementData').filter((element) => {
+    const filteredData = this.get('filteredData').filter((element) => {
       const searchTerm = filterValue.trim().toLowerCase();
       return (
         element.name.toLowerCase().includes(searchTerm) ||
@@ -42,9 +55,11 @@ export class PeriodicTableStateService extends RxState<PeriodicTableState> {
   }
 
   updateDataSource(updatedElement: PeriodicElement) {
-    const updatedData = this.get('periodicElementData').map((element) =>
+    const updatedData = this.get('filteredData').map((element) =>
       element.position === updatedElement.position ? updatedElement : element
     );
-    this.set({ periodicElementData: updatedData });
+    this.set({ filteredData: updatedData });
+    console.log(this.get('periodicElementData'))
+    console.log(this.ELEMENT_DATA)
   }
 }
